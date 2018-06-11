e656c0f1c11ef1aa7937e32e77f9cebca5b9b4c1
travis joe <weelgod@gmail.com>
2018-06-12 06:31:51 +0800

    init

---
+++ blockchain/myBlockChain.py
@@ -0,0 +1,12 @@
+# {
+#     index: 0,
+#     timestamp: 12312314324,
+#     transaction: [
+#       {
+#             sender:'',
+#             recipient: '',
+#             amount: '',
+#     ],
+#     proof: "",
+#     "previous_hash": ""
+# }
