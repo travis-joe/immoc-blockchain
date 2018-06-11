bdfef8ca7a896d70e76a0995ea989878ebddde4e
travis joe <weelgod@gmail.com>
2018-06-12 07:06:35 +0800

    3-4

--- blockchain/myBlockChain.py
+++ blockchain/myBlockChain.py
@@ -10,3 +10,42 @@
 #     proof: "",
 #     "previous_hash": ""
 # }
+import hashlib
+import json
+from time import time
+
+
+class BlockChain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.new_block(proof=100, previous_hash=1)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block)
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount) -> int:
+        self.current_transactions.append({
+            sender,
+            recipient,
+            amount
+        })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_key=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
