ddbbd1e20e7582bb2840eec001a6f27f9ae88f5c
travis joe <weelgod@gmail.com>
2018-06-13 08:30:16 +0800

    my-pet-shop

---
+++ my-pet-shop/test/TestAdoption.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.0;
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+}
